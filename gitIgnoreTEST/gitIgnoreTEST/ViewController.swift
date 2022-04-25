2ea019c840477d600cb0c132f990c8c2551262ae
doogie97 <chris_97@naver.com>
2022-04-25 17:47:49 +0900

    xcode만들었음

---
+++ gitIgnoreTEST/gitIgnoreTEST/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gitIgnoreTEST
+//
+//  Created by 최최성균 on 2022/04/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
